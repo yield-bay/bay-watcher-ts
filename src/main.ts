0582c4b0c63471c6674887be653c0983abe6c421
Jack Sparrow <capjacksparrow42@protonmail.com>
2022-11-24 13:56:19 +0530

    run karura task every 5min

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { runKaruraTask } from "./tasks/karura";
 
 const main = async () => {
     await runKaruraTask();
+    setInterval(runKaruraTask, 1000 * 60 * 5) // every 5min
 }
 
 connectToDatabase().then(() => {
