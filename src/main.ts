aa6d83155ae5bb3f0b257ac98c6dcc2ae9eb0f25
Jack Sparrow <capjacksparrow42@protonmail.com>
2022-12-10 10:53:18 +0530

    use setInterval once

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,23 @@ import { runMangataTask } from "./tasks/mangata";
 import { runSiriusTask } from "./tasks/sirius";
 import { runArswTask } from "./tasks/arsw";
 
-const main = async () => {
+const job = async () => {
     await runArswTask();
     await runSiriusTask();
     await runMangataTask();
     await runKaruraTask();
-    setInterval(runArswTask, 1000 * 60 * 5) // every 5min
-    setInterval(runSiriusTask, 1000 * 60 * 5) // every 5min
-    setInterval(runMangataTask, 1000 * 60 * 5) // every 5min
-    setInterval(runKaruraTask, 1000 * 60 * 5) // every 5min
+}
+const main = async () => {
+    await job();
+    // await runArswTask();
+    // await runSiriusTask();
+    // await runMangataTask();
+    // await runKaruraTask();
+    // setInterval(runArswTask, 1000 * 60 * 5) // every 5min
+    // setInterval(runSiriusTask, 1000 * 60 * 5) // every 5min
+    // setInterval(runMangataTask, 1000 * 60 * 5) // every 5min
+    // setInterval(runKaruraTask, 1000 * 60 * 5) // every 5min
+    setInterval(job, 1000 * 60 * 5);
 }
 
 connectToDatabase().then(() => {
